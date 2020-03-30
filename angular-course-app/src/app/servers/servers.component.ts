4f4a018197d203096635ad54c2e10edaafa0fab6
Chris Savill <chris.l.savill@gmail.com>
2020-03-30 10:56:59 +0100

    Data binding covered. String interpolation using {{ propertyName }} syntax. Property binding using [attribute]="propertyName" syntax. Event binding using (eventName)="functionName" syntax. Two-way binding using [(ngModel)]="propertyName" syntax.

--- angular-course-app/src/app/servers/servers.component.ts
+++ angular-course-app/src/app/servers/servers.component.ts
@@ -4,17 +4,28 @@ import { Component, OnInit } from '@angular/core';
   //selector: 'app-servers', // Using a HTML element selector, preferred for components
   //selector: '[app-servers]', // Using an attribute selector
   selector: '.app-servers', // Using a class selector
-  //templateUrl: './servers.component.html', // Using an external file is preferred.
-  template: `
+  templateUrl: './servers.component.html', // Using an external file is preferred.
+  /* template: `
   <app-server></app-server>
-  <app-server></app-server>`, // The backtick (`) allows for multiline strings.
+  <app-server></app-server>`, // The backtick (`) allows for multiline strings. */
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit {
+export class ServersComponent {
+  allowNewServer = false; // Used in property binding via [attribute]="propertyName" syntax.
+  serverCreationStatus = 'No new server was created.';
+  serverName = 'Default';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  onCreateServer() { // Used in event binding via (eventName)="functionName" syntax.
+    this.serverCreationStatus = 'Server ' + this.serverName + ' was created.';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value; // Casting required to ensure TypeScript knows what type on HTML element we are dealing with.
+  }
 }
